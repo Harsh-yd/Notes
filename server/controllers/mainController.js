d82cd2e20f104ce73f0b03430d0f5c2ef1de87a9
Harsh-yd <132829664+Harsh-yd@users.noreply.github.com>
2023-07-31 03:39:21 +0530

    Add files via upload

---
+++ server/controllers/mainController.js
@@ -0,0 +1,28 @@
+/**
+ * GET /
+ * Homepage 
+*/
+exports.homepage = async (req, res) => {
+  // // res.send('Hello how are yu');
+  const locals = {
+    title: "NodeJs Notes",
+    description: "Free NodeJS Notes App.",
+  }
+  res.render('index', {
+    locals,
+    layout: '../views/layouts/front-page'
+  });
+}
+
+
+/**
+ * GET /
+ * About 
+*/
+exports.about = async (req, res) => {
+  const locals = {
+    title: "About - NodeJs Notes",
+    description: "Free NodeJS Notes App.",
+  }
+  res.render('about', locals);
+}
